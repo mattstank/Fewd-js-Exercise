f38787fdc6ecc1b3c28f9f0f1fdba9ed49016170
Matt Stankiewicz <mattystankart@gmail.com>
2018-04-03 18:01:05 -0400

    finished javascript practice in one loop

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 (function () {
 
+const buttons = document.querySelectorAll("button");
+
+  buttons[0].addEventListener('click', function(event) {
+        event.target.classList.toggle('button-danger')
+  });
+    
+    
+    buttons[1].addEventListener('click',function(event) {
+        event.target.innerText = 'You have clicked me';
+})
+
+    
+    buttons[2].addEventListener('click',function() {
+      buttons[1].innerText = 'button 2';
+})
   // your js goes here
 
-})();
+})()
